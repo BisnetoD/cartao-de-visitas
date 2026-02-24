f6c4c5bc8efbd1907fe8cb07dd331e985416b00f
Fernando Issler <fernandoissler@hotmail.com>
2026-02-23 21:33:01 -0300

    Commit inicial

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// apenas um JS: ao clicar, aparece um alert
+document.getElementById("btnContato").addEventListener("click", function () {
+    alert("Olá! Me chame no WhatsApp ou envie um e-mail 🙂");
+});
+
+// extra simples só pra atualizar o ano (não precisa clicar)
+document.getElementById("year").textContent = new Date().getFullYear();
